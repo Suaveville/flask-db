4a8bce480e58a3aeac9b551a2f04250f3f046158
Suaveville <spiderpig_mgo@hotmail.com>
2021-03-18 01:15:15 -0700

    init commit

---
+++ application.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template,jsonify
+import aws_controller
+
+
+
+application = Flask(__name__)
+
+# Index
+@application.route('/')
+def index():
+    return render_template('home.html')
+
+@application.route('/get-items')
+def get_items():
+    return jsonify(aws_controller.get_items())
+
+# About
+@application.route('/about')
+def about():
+    return render_template('about.html')
+
+# Articles
+@application.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+
+# Articles
+#@app.route('/articles')
+#def articles():
+
+# Con esto debugeamos
+if __name__ == '__main__':
+    
+    application.run(debug=True)
